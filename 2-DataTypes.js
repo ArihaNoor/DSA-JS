5bf23375bdd9008b5816390fd34d59bf158c1ebf
Ariha Noor <arihanoor0@gmail.com>
2024-07-09 10:39:12 +0500

    2-DataTypes

--- 2-DataTypes.js
+++ 2-DataTypes.js
@@ -1,4 +1,20 @@
 // There are three types of variables in JS 
 // let, var, const 
+// -- const -- 
+// Value is locked, we cannot change the value after it is assigned on time.
+
 // -- let -- 
-// variables declared using let keyword can be accessed in the functional scope
+// let is only {} scope we use it now.
+
+// -- var -- 
+// works on the block level scope, prefer not to use var 
+
+const name = "Ariha";
+console.log(name);
+var age = 20;
+let grade = "A";
+console.log(grade);
+console.log(age);
+
+let a;
+console.log(a);  //if not initialized than it will be undefined.
