8af30c51d1520dc5c1b54dc7939cbc2de1c65946
Ariha Noor <arihanoor0@gmail.com>
2024-07-09 15:20:35 +0500

    time-complexity

---
+++ 8-TimeComplexity.js
@@ -0,0 +1,15 @@
+// What is Time Complexity?
+// Time Complexity is not equal to the Time taken to run a program 
+//Time complexity is defined as the rate at which time taken increases with respect to the input size.
+//----Big O() Notation---
+
+//Rules for Optimization
+// 1- Avoid Constants 
+// like if we have a O(Nx3 + 1) Here 1 is constant value which do not have any effect on the time complexity so we will not consider it.
+// 2- Always compute time complexity considering worst case scnerio
+// 3- Avoid Lower Values -- like don't consider exponential values which are very smaller and have no signifcance.
+ 
+// There are 3 cases: Best case, Worst Case and Average Case
+
+
+
