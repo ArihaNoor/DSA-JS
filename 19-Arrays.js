29812bd2e6b3871fdcf2196abe9146ca37625c90
Ariha Noor <arihanoor0@gmail.com>
2024-08-26 21:47:59 +0500

    JS

--- 19-Arrays.js
+++ 19-Arrays.js
@@ -1,8 +1,49 @@
-const myArr = [0,2,3,4,44,8, "true", "Ariha"]
+// int array = [1,3,4,5,8]
+
+// const myArr = [0,2, "true", "Ariha"]
+
+
+const Person = [
+    {
+        name: "Ariha",
+        age: 21   
+    },
+    {
+        name: "Ariha",
+        age: 21   
+    },
+    {
+        name: "Ariha",
+        age: 21   
+    }
+]
+
+
+
+
+// Person.forEach((person)=>{console.log(person)})
+
+
+// for(let i=0;i<myArr.length;i++){
+//     console.log(myArr[i])
+// }
+
+
+
 
 // console.log(myArr[2])
 
-const myArr2 = new Array(1,2,33,4)
+
+// const number = 1
+
+// const number2 = new Number()
+
+// const myArrayObject = new Array();
+
+
+
+// const myArr2 = new Array(1,2,33,4)
+
 
 
 
@@ -13,11 +54,15 @@ const myArr2 = new Array(1,2,33,4)
 // In javascript we don't have associative  
 // Associative array is like objects like we have key and value 
 
+
+
 //---------Zero based Indexing-----------
 
 // Shallow Copy and Deep Copy 
 //Shallow copy share same reference point change in original 
 //Deep copy change in copy not in original --- properties do not share the same reference 
+
+
 
 // Array Methods
 
@@ -32,25 +77,63 @@ const myArr2 = new Array(1,2,33,4)
 //Slice-- returns section of an array it does not effect the original array
 //splice-- it removes the array elements from an original array 
 
-//--------Spread Operator---------
-const arr = ["Ariha", 1,2,3,4]
 
+// const myArr = [0,2, "true", "Ariha"]
+
+// console.log("Includes: " , myArr.includes("Ariha"))
+
+// console.log("Index: " , myArr.indexOf("Ariha"))
+
+// const newArrayJoin = myArr.join("-")
+
+// console.log(newArrayJoin)
+
+// console.log("Original Array: ", myArr)
+
+// const SlicedArray = myArr.slice(1,3)
+
+// console.log("Sliced Array", SlicedArray)
+
+// console.log("Original Array: ", myArr)
+
+// const SplicedArray = myArr.splice(1,3)
+
+// console.log("Spliced Array", SplicedArray)
+
+// console.log("Original Array: ", myArr)
+
+
+
+
+// //--------Spread Operator---------
+// const arr = ["Ariha", 1,2,3,4]
+// console.log(arr)
 // console.log(...arr)
+
+
+
 
 
 //------------Flat Method---------------
 // let suppose we have an array inside array like nested arrays than we can make this into one array 
 
-const newArray = [1,2,3,4,[1,2,3,4,[1,2,3,4,4]]]
+// const newArray = [1,2,3,4,[1,2,3,4,[1,2,3,4,4]]]
+
+// console.log(newArray)
 
 // console.log(newArray.flat(Infinity))
 
 
+
+
+
 //-----------Array from--------
 
-const newArr = Array.from("Ariha")
+// const name = "Ariha Noor"
 
-// console.log(newArr)
+// const newArr = Array.from(name)
+
+// console.log(newArr.length)
 
 
 
