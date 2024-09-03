ff7ae9273b46c8a2794d93cbaf318a2a09000730
Ariha Noor <arihanoor0@gmail.com>
2024-09-03 18:29:21 +0500

    IIFE

---
+++ 24-IIFE.js
@@ -0,0 +1,23 @@
+// Immediately Invoked Function Expressions ----------- IIFE
+
+// Sometimes global scope values make local or block scope values polluted 
+// because as we know that block level variables can access the globals scope values 
+
+// For Immediate Execution of the Function --- like we want some function to execute immediately 
+// as file loads
+
+//--------How we can write IIFE----------
+
+(function MyFunction(){
+    console.log("DB Connected")
+})();
+
+
+//-----Here we need to add semicolon because it doesnot implicity know where scope will end
+
+//------Same we can do with Arrow Functions----------
+
+((name)=>{
+    console.log("DB Connected TWO", name)
+})("Ariha")
+
