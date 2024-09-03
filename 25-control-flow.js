d50e4b794996c3b7dfb89ad3815b25204daf84eb
Ariha Noor <arihanoor0@gmail.com>
2024-09-03 19:58:24 +0500

    control-flow

---
+++ 25-control-flow.js
@@ -0,0 +1,43 @@
+//---------If-Else 
+//---------IF-Else-If
+
+//== and === Difference 
+
+//!= and ==,  !==
+
+//----------Short Hand Notation 
+
+// if(age>10) console.log("Young") //not readable 
+
+//--------&&------------
+
+
+//---------||----------
+
+
+//--------Conditional Rendering--------------
+
+
+//---------Falsy Values 
+
+// 0, False, -0, 0n, BigInt, null, undefined, Nan, ""
+
+//--------Truthy Values 
+
+// true, 1, "0", "false", " ", [], {}, function(){}
+
+
+//-------How to Check Array and Object are emmpty or not 
+
+const Array = []
+// console.log(Array.length)
+
+const ObjectNew = {
+    // username: "ariha"
+}
+// console.log(Object.keys(ObjectNew).length);
+
+
+
+
+
