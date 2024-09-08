c129927c712e0d23167e1f8d7fed6774ce3c920a
Ariha Noor <arihanoor0@gmail.com>
2024-09-08 19:14:56 +0500

    foreach

--- 28-foreach.js
+++ 28-foreach.js
@@ -13,5 +13,30 @@ let values = myCoding.forEach((item)=>{
 const mynums = [1,2,3,4,5,6,7,8,9,10]
 
 mynums.forEach((num)=>{
-    console.log(num)
+    // console.log(num)
 })
+
+//foreach function will not return any value only for printing
+
+const filteredNums = mynums.filter((num)=>{
+    return num>4;
+})
+
+console.log(filteredNums)
+
+// so where we have to return we use filter function 
+
+
+//----------Implicit Return-----------
+
+
+const Arrow = () => mynums[0]
+
+const Arrow2 = () => (mynums[0])
+
+
+//----------Explicit Return-----------------
+
+const Arrow3 = () => {
+    return mynums[0];
+}
